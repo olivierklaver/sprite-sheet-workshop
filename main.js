05507fdabd2d74195731f3de136dfeff9e01538a
olivierklaver <olivierklaver@users.noreply.github.com>
2018-11-26 13:25:09 +0100

    Add animation

--- main.js
+++ main.js
@@ -4,5 +4,12 @@ let char = document.querySelector(".character");
 // set the frame
 let frame = 0;
 
-// move background image to frame position
-char.style.cssText = "background-position-x: "+ (frame * -103) +"px";
+setInterval(draw, 100);
+
+function draw(){
+	// move background image to frame position
+	char.style.cssText = "background-position-x: "+ (frame * -103) +"px";
+
+	frame++;
+	if(frame > 12) frame = 3;
+}
